9c509f4aff30b8c63e96bb33e646622ce7a87d0e
azotGr <alinazotova721@gmail.com>
2024-06-08 15:44:38 +0300

    1/2

--- app/src/main/java/com/example/task_8_finaly/MainActivity.kt
+++ app/src/main/java/com/example/task_8_finaly/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.example.task_8_finaly
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val buttonSr = findViewById<Button>(R.id.buttonSearch)
+        val buttonSrClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Найди меня", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonSr.setOnClickListener(buttonSrClickListener)
+
+        val buttonMedia = findViewById<Button>(R.id.buttonMedia)
+
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Тунь-тунь", Toast.LENGTH_SHORT).show()
+        }
+
+        val buttonSetting = findViewById<Button>(R.id.buttonSetting)
+
+        buttonSetting.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Подожди чу чут", Toast.LENGTH_SHORT).show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
