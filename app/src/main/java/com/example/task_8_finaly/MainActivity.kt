34f6454735cf916f599b1838a5b0484116c1360e
azotGr <alinazotova721@gmail.com>
2024-06-09 13:30:56 +0300

    2.2/2

--- app/src/main/java/com/example/task_8_finaly/MainActivity.kt
+++ app/src/main/java/com/example/task_8_finaly/MainActivity.kt
@@ -33,28 +33,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(displayIntent)
         }
 
-/*
-        val buttonSr = findViewById<Button>(R.id.buttonSearch)
-        val buttonSrClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Найди меня", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        buttonSr.setOnClickListener(buttonSrClickListener)
-
-        val buttonMedia = findViewById<Button>(R.id.buttonMedia)
-
-        buttonMedia.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Тунь-тунь", Toast.LENGTH_SHORT).show()
-        }
-
-        val buttonSetting = findViewById<Button>(R.id.buttonSetting)
-
-        buttonSetting.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Подожди чу чут", Toast.LENGTH_SHORT).show()
-        }
-*/
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
