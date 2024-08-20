f316e5f0374dec84457f20e86744c14c4bc6b359
azotGr <alinazotova721@gmail.com>
2024-08-20 19:08:11 +0300

    9.2

--- app/src/main/java/com/example/task_8_finaly/MainActivity.kt
+++ app/src/main/java/com/example/task_8_finaly/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.task_8_finaly
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 //import android.view.View
--- app/src/main/java/com/example/task_8_finaly/SettingsActivity.kt
+++ app/src/main/java/com/example/task_8_finaly/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.task_8_finaly
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.switchmaterial.SwitchMaterial
@@ -21,8 +24,9 @@ class SettingsActivity : AppCompatActivity() {
 
         val backButton: ImageButton = findViewById(R.id.buttonImage)
         backButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            //val intent = Intent(this, MainActivity::class.java)
+            //tartActivity(intent)
+            //зачем мы делаем переход, если из-за него будет наложение активити
             finish()
         }
 
@@ -40,5 +44,43 @@ class SettingsActivity : AppCompatActivity() {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
         }
+
+        //BUTTON SHARE
+
+        val shareImageButton: ImageView = findViewById(R.id.imageRepost)
+        shareImageButton.setOnClickListener {
+            val shareIntent = Intent().apply{
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.yandex_curses))
+                type = "text/plain"
+            }
+
+            val chooser = Intent.createChooser(shareIntent, null)
+            startActivity(chooser)
+        }
+
+        //BUTTON SUPPORT
+
+        val supportImageButton: ImageView = findViewById(R.id.imageSupport)
+        supportImageButton.setOnClickListener{
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.title_email))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.text_email))
+            }
+            val chooser = Intent.createChooser(supportIntent, null)
+            startActivity(chooser)
+        }
+
+        //BUTTON DOCUMENTS
+
+        val userAgreementButton: ImageView = findViewById(R.id.imageRight)
+        userAgreementButton.setOnClickListener {
+            val url = getString(R.string.offer)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
     }
 }
